59b28c1158507531cd9fb026442a193dfa5372be
Sarah Marcus <sarahhm21@gmail.com>
2020-12-06 18:13:51 -0800

    Initial Commit

---
+++ CountryList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryList
+//
+//  Created by Sarah Marcus on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
